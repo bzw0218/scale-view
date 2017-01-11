b8bbc56b6c28d4fcd3700e6c3259975a8fb48f02
bizhiwei <1291947405@qq.com>
2017-01-11 18:55:49 +0800

    fix

--- js/scaleView.js
+++ js/scaleView.js
@@ -3,11 +3,26 @@
  */
 !(function(window,document){
     var scaleView=(function(){
-        var picList=[];
+        var picList=[];//图片列表
+        var picIndex=0;//列表中的第几个图片
+
+        /* pic 对象
+         {
+         link: "http://img3.cache.netease.com/f2e/ent/ent_painting2016/images/2.jpg?1520",
+         imgW: "1875",
+         imgH: "3015",
+         areaW: "375",
+         areaH: "603",
+         areaL: "1379",
+         areaT: "103",
+         limitMax: .3,
+         limitMin: .2
+         }
+        * */
 
         function config(List){
             picList=List;
-            //start();
+            picIndex=0;
         }
 
         function start(){
